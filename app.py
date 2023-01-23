4d234198f24058683a097afa63579effe8bab06d
Seksak <77098302+pseksa@users.noreply.github.com>
2023-01-23 14:59:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-import streamlit as st 
-st.title("BTCUSD sentiment ")
-st.header("Hello World 👏")
-
+import streamlit as st
+import plost
+import pandas as pd 
+#st.header("BTCUSD Price Prediction")
+st.title('BTCUSD Price Prediction')
 st.write("This is my first app")
+df = pd.read_csv('https://raw.githubusercontent.com/pseksa/webapp/main/BTC-USD.csv')
+st.write (df)
+st.line_chart(df['Close'])
+
+
+plost.line_chart(
+    data=df,
+    x='Date',
+    y=('Open', 'Close', 'High', 'Low'))
